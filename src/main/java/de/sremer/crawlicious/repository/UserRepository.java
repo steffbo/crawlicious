4f06e99319c3432cb9ebc9386afc1bf9cad299ea
Stefan Remer <stefan.remer@gmail.com>
2017-10-11 23:50:58 +0200

    whitespace

--- src/main/java/de/sremer/crawlicious/repository/UserRepository.java
+++ src/main/java/de/sremer/crawlicious/repository/UserRepository.java
@@ -9,5 +9,6 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findUserByEmail(String email);
+
     List<User> findUserByName(String name);
 }
