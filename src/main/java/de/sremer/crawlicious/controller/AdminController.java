2ab151546374b2e4fa30febf0ef2ff5336fe528a
Stefan Remer <stefan.remer@gmail.com>
2017-10-11 23:40:28 +0200

    added admin controller

---
+++ src/main/java/de/sremer/crawlicious/controller/AdminController.java
@@ -0,0 +1,44 @@
+package de.sremer.crawlicious.controller;
+
+import de.sremer.crawlicious.model.User;
+import de.sremer.crawlicious.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Set;
+
+@Controller
+public class AdminController {
+
+    static final Logger LOG = LoggerFactory.getLogger(AdminController.class);
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = {"/admin"}, method = RequestMethod.GET)
+    public ModelAndView adminHome() {
+        ModelAndView modelAndView = new ModelAndView();
+        User currentUser = userService.getCurrentUser();
+        modelAndView.addObject("userName", "Welcome " + currentUser.getName() + "!");
+        modelAndView.addObject("adminMessage", "Content Available Only for Users with Admin Role");
+        modelAndView.setViewName("admin/home");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = {"/test"}, method = RequestMethod.GET)
+    public ModelAndView test(HttpServletRequest request, Model model) {
+        ModelAndView modelAndView = new ModelAndView();
+        Set<User> lastUsers = userService.listLastUsers(10);
+        modelAndView.addObject("users", lastUsers);
+        modelAndView.setViewName("test");
+        return modelAndView;
+    }
+
+}
