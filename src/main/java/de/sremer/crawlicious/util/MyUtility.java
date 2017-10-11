0ded1d847f9ed25c1cd2e5457834258ddaf409a1
Stefan Remer <stefan.remer@gmail.com>
2017-10-11 23:43:26 +0200

    split tags with regex

---
+++ src/main/java/de/sremer/crawlicious/util/MyUtility.java
@@ -0,0 +1,32 @@
+package de.sremer.crawlicious.util;
+
+import de.sremer.crawlicious.model.Tag;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MyUtility {
+
+    /**
+     * Parses a string that the users entered as the tags for a posting.
+     * Tags can be separated by almost everything except _
+     *
+     * @param userInputTags
+     * @return Set of Tags
+     */
+    public static Set<Tag> parseTags(String userInputTags) {
+
+        Pattern pattern = Pattern.compile("(\\w*)*");
+        Matcher matcher = pattern.matcher(userInputTags);
+        Set<Tag> tagList = new HashSet<>();
+        while (matcher.find()) {
+            String tagString = matcher.group(0);
+            if (!tagString.isEmpty()) {
+                tagList.add(new Tag(tagString));
+            }
+        }
+        return tagList;
+    }
+}
