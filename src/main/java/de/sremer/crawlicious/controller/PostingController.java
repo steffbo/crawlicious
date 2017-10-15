8451a09f8fd5e35fdfbed75ab6d0204570ae1293
Stefan Remer <stefan.remer@gmail.com>
2017-10-15 16:20:27 +0200

    update postings

--- src/main/java/de/sremer/crawlicious/controller/PostingController.java
+++ src/main/java/de/sremer/crawlicious/controller/PostingController.java
@@ -3,6 +3,7 @@ package de.sremer.crawlicious.controller;
 import de.sremer.crawlicious.model.Posting;
 import de.sremer.crawlicious.model.User;
 import de.sremer.crawlicious.service.PostingService;
+import de.sremer.crawlicious.service.TagService;
 import de.sremer.crawlicious.service.UserService;
 import de.sremer.crawlicious.util.MyUtility;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +19,13 @@ public class PostingController {
 
     private PostingService postingService;
     private UserService userService;
+    private TagService tagService;
 
     @Autowired
-    PostingController(PostingService postingService, UserService userService) {
+    PostingController(PostingService postingService, UserService userService, TagService tagService) {
         this.postingService = postingService;
         this.userService = userService;
+        this.tagService = tagService;
     }
 
     @GetMapping(value = "/insert")
@@ -40,7 +43,7 @@ public class PostingController {
             @RequestParam(value = "link", required = true) String link,
             @RequestParam(value = "tags", required = true) String tags) {
 
-        Posting posting = new Posting(title, link, MyUtility.parseTags(tags));
+        Posting posting = new Posting(title, link, MyUtility.parseTags(tagService, tags));
         User currentUser = userService.getCurrentUser();
         posting.setUser(currentUser);
         postingService.insertPosting(posting);
@@ -50,27 +53,65 @@ public class PostingController {
         return profile;
     }
 
-    @RequestMapping(value = "/update/{id}", method = RequestMethod.GET)
+    @PostMapping(value = "/update")
+    public ModelAndView updatetPosting(
+            @RequestParam(value = "id", required = true) String id,
+            @RequestParam(value = "title", required = true) String title,
+            @RequestParam(value = "link", required = true) String link,
+            @RequestParam(value = "tags", required = true) String tags) {
+
+        Posting posting = postingService.getPostingById(Long.valueOf(id));
+        posting.setTitle(title);
+        posting.setLink(link);
+        posting.setPostingTags(MyUtility.parseTags(tagService, tags));
+        postingService.insertPosting(posting);
+
+        return new ModelAndView("redirect:/profile");
+    }
+
+    @GetMapping(value = "/update/{id}")
     public ModelAndView updatePosting(@PathVariable("id") int id) {
         ModelAndView modelAndView = new ModelAndView();
         User currentUser = userService.getCurrentUser();
+        Posting posting = postingService.getPostingById(id);
         modelAndView.addObject("user", currentUser);
-        modelAndView.addObject("posting", postingService.getPostingById(id));
-        modelAndView.setViewName("posting");
+        modelAndView.addObject("id", id);
+        modelAndView.addObject("title", posting.getTitle());
+        modelAndView.addObject("link", posting.getLink());
+        modelAndView.addObject("tags", MyUtility.getStringFromTags(posting.getPostingTags()));
+        modelAndView.setViewName("posting_update");
         return modelAndView;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @PostMapping(value = "/update/tag/add/")
+    public ModelAndView addTag(
+            @RequestParam(value = "postingId", required = true) String postingId,
+            @RequestParam(value = "tag", required = true) String tag) {
+
+        postingService.addTagToPosting(Long.valueOf(postingId), tag);
+        return new ModelAndView("redirect:/profile");
+    }
+
+    @PostMapping(value = "/update/tag/remove/")
+    public ModelAndView removeTag(
+            @RequestParam(value = "postingId", required = true) String postingId,
+            @RequestParam(value = "tagId", required = true) String tagId) {
+
+        postingService.removeTagFromPosting(Long.valueOf(postingId), Long.valueOf(tagId));
+        return new ModelAndView("redirect:/profile");
+    }
+
+    @GetMapping
     public Collection<Posting> getPostings() {
         return postingService.getPostings();
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "/{id}")
     public Posting getPostingById(@PathVariable("id") int id) {
         return postingService.getPostingById(id);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping(value = "/{id}")
     public void deletePostingById(@PathVariable("id") int id) {
         postingService.deletePostingById(id);
     }
