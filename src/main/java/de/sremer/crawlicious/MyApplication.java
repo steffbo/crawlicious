f8b3c979608a877e598762f009221551a2cff0cf
Stefan Remer <stefan.remer@gmail.com>
2017-11-27 22:11:04 +0100

    enable async for mail sending

--- src/main/java/de/sremer/crawlicious/MyApplication.java
+++ src/main/java/de/sremer/crawlicious/MyApplication.java
@@ -2,11 +2,28 @@ package de.sremer.crawlicious;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
+@EnableAsync
 public class MyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+    @Bean
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("BGThread-");
+        executor.initialize();
+        return executor;
+    }
 }
