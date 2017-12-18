4ea744b6da71fc9973173f3cc67eca530bfc5338
Stefan Remer <stefan.remer@gmail.com>
2017-12-18 07:31:23 +0100

    receive title from link

--- src/main/java/de/sremer/crawlicious/util/MyUtility.java
+++ src/main/java/de/sremer/crawlicious/util/MyUtility.java
@@ -3,7 +3,11 @@ package de.sremer.crawlicious.util;
 import de.sremer.crawlicious.model.Tag;
 import de.sremer.crawlicious.service.TagService;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,14 +17,14 @@ public class MyUtility {
 
     /**
      * Parses a string that the users entered as the tags for a posting.
-     * Tags can be separated by ^ . , ; _ * ~ + - \ / & and space.
+     * Tags can be separated by , ; ~ & and space.
      *
      * @param userInputTags
      * @return Set of Tags
      */
     public static Set<Tag> parseTags(TagService tagService, String userInputTags) {
 
-        Pattern pattern = Pattern.compile("([^.,;_*~+-\\/& ]*)*");
+        Pattern pattern = Pattern.compile("([^,;~& ]*)*");
         Matcher matcher = pattern.matcher(userInputTags);
         Set<Tag> tagList = new HashSet<>();
         while (matcher.find()) {
@@ -35,5 +39,29 @@ public class MyUtility {
 
     public static String getStringFromTags(Set<Tag> tags) {
         return tags.stream().map(Tag::getName).collect(Collectors.joining(" "));
+    }
+
+    public static String getWebsiteTitle(String url) {
+        InputStream response = null;
+        try {
+            response = new URL(url).openStream();
+            Scanner scanner = new Scanner(response);
+            String responseBody = scanner.useDelimiter("\\A").next();
+            String title = responseBody.substring(responseBody.indexOf("<title>") + 7, responseBody.indexOf("</title>"));
+            return title;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                response.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getWebsiteTitle("https://stackoverflow.com/questions/40099397"));
     }
 }
